551b71bb2d5bfc4964d9b3f7772c0f14685b239a
Heliot <heliotvilchis@gmail.com>
2021-05-12 10:21:10 -0400

    ok

--- src/pages/nouvelles.js
+++ src/pages/nouvelles.js
@@ -12,7 +12,17 @@ const NouvellesPage = () => {
   return (
     <Layout>
       <Container>
-        <h1>Nouvelles</h1>
+        <article>
+          <h1>Nouvelles</h1>
+          <section>
+            <p>9 Septembre 2020</p>
+            <p>
+              Considérant la grande demande, pour un chiot ou visite,
+              présentement je ne prendrai aucune réservation ni visite ne serons
+              autorisé avant un avis contraire.
+            </p>
+          </section>
+        </article>
       </Container>
     </Layout>
   );
