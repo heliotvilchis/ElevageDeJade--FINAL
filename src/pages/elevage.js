7be224ca0bfb35de9cec70b36947a0e6f2de8a4d
Heliot <heliotvilchis@gmail.com>
2021-06-01 18:45:15 -0400

    ok

--- src/pages/elevage.js
+++ src/pages/elevage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 // Components
 import Container from "../components/container/container";
@@ -7,10 +8,16 @@ import Card from "../components/card/card";
 
 // Images
 import i1 from "../assets/images/gabou/1.jpg";
+import i2 from "../assets/images/rapsodie/1.jpg";
+import i3 from "../assets/images/tina-turner/1.webp";
+import i4 from "../assets/images/india/1.jpg";
+import i5 from "../assets/images/renagade/1.webp";
+import i6 from "../assets/images/soft-shiny/1.jpg";
 
 const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-between;
 `;
 
 // Markup
@@ -30,16 +37,26 @@ const Elevage = () => {
           Je vous invite à visiter la page des mâles et femelles pour plus
           d'informations sur chacun des chiens et plus de photos.
         </p>
-        <Card
-          image={i1}
-          dname="CH. DeJade Soft Dream in Gold (Gabou)"
-          gender="m"
-        />
-        <Card
-          image={i1}
-          dname="DeJade Sweet Rhapsodie in Blue (Sweety)"
-          gender="f"
-        />
+        <CardContainer>
+          <Card
+            image={i1}
+            dname="CH. DeJade Soft Dream in Gold (Gabou)"
+            gender="m"
+          />
+          <Card
+            image={i2}
+            dname="DeJade Sweet Rhapsodie in Blue (Sweety)"
+            gender="f"
+          />
+          <Card image={i3} dname="DeJade Just Call Me Tina Turner" gender="f" />
+          <Card
+            image={i4}
+            dname="CH. DeJade Star Of Cassy (India)"
+            gender="f"
+          />
+          <Card image={i5} dname="DeJade Renagade Gracious White" gender="f" />
+          <Card image={i6} dname="DeJade Soft Shiny" gender="f" />
+        </CardContainer>
       </Container>
     </Layout>
   );
