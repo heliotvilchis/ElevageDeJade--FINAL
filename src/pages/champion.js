26d07ec77258bada77bdd59195a28a23cd050458
Heliot <heliotvilchis@gmail.com>
2021-06-01 19:24:48 -0400

    Create champion.js

---
+++ src/pages/champion.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Components
+import Container from "../components/container/container";
+import Layout from "../components/layout/layout";
+
+// Markup
+const Champions = () => {
+  return (
+    <Layout>
+      <h1>champions</h1>
+    </Layout>
+  );
+};
+
+export default Champions;
