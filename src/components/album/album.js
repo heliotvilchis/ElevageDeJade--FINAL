07c7d442cd74a93e779d5e064d6f2d0e28b0b205
Heliot <heliotvilchis@gmail.com>
2021-06-04 12:21:09 -0400

    ok

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,2 +1,18 @@
 import React from "react";
 import { graphql } from "gatsby";
+
+class FullCalendar extends React.Component {
+  componentDidMount() {
+    console.log("page rendered");
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>all the photos</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FullCalendar;
---
+++ src/pages/albums/gabou.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Assets
+import Contianer from "../../components/container/container";
+import AlbumComp from "../../components/album/album";
+import Layout from "../../components/layout/layout";
+
+// Markup
+const Album = () => {
+  return (
+    <Layout>
+      <Contianer>
+        <h1>the album</h1>
+      </Contianer>
+    </Layout>
+  );
+};
+
+export default Album;
